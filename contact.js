64693634e0ce3b5b9660ec17bc760e76da87f841
Devin Hamilton <devlinx76@icloud.com>
2023-04-11 21:17:17 -0400

    Added Contact JS

--- contact.js
+++ contact.js
@@ -1,39 +1,22 @@
-<!DOCTYPE html>
-<html lang="en-US">
-<head>
-    <!-- styles for all pages -->
- 
-    <link rel="stylesheet" href="index.css" />
-</head>
-<body>
-    <header>
-    <span class="name">Devin Hamilton</span>
-<nav class="Links">
-    <div class="button-group">
-        <a class="active" href="index.html">HOME</a>
-        <a class="active" href="about.html">ABOUT</a>
-        <a class="active" href="portfolio.html">PORTFOLIO</a>
-        <a class="active" href="contact.html">CONTACT</a>
-    </div>
+console.log('We are connected!')
 
-</nav>
-    </header>
-    <main>
+const form = document.querySelector("#form")
+const submitButton = document.querySelector("#submit")
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxLZTlgHBunzjDAyvzTFB9AAQZS2vGT4hSZKlfMDVMQrrHEQwKDYO2-fo05BNfFegYWVg/exec'
 
-    </main>
-    <footer>
-        <a href="https:facebook.com" target="_blank">
-            <img class="media-link" src="images/facebook.svg" alt="">
-        </a>
-        <a href="https://twitter.com" target="_blank">
-            <img class="medica-link" src="images/twitter.svg" alt="">
-        </a>
-        <a href="https:linkedin.com" target="_blank">
-            <img class="media-link" src="images/linkedin.svg" alt="">
-        </a>
-        </footer>
-        <body>
-            <img src="https://vscode.dev/github/DevLinx76/Personal-website/blob/5a6ac1997f4303d629864437a0b5a3d9532caa68/Images/night-sky-milky-way-galaxy.jpeg" alt="">
+form.addEventListener('submit', e => {
+  submitButton.disabled = true
+  e.preventDefault()
+  let requestBody = new FormData(form)
+  fetch(scriptURL, { method: 'POST', body: requestBody})
+    .then(response => {
+       alert('Success!', response)
+       submitButton.disabled = false
+      })
+    .catch(error => {
+    alert('Error!', error.message)
+      submitButton.disabled = false
 
-        </body>
-</body>
+    }
+    )
+})
